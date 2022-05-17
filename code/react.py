0c6189390b135379cb596ca5b8cbe548791b75f7
Uri Klar <uriklar@gmail.com>
2022-05-17 10:11:30 +0300

    add look as an alias to hunt

---
+++ code/react.py
@@ -0,0 +1,12 @@
+# from talon import  Module, Context, actions 
+# import subprocess
+# import os
+# import pathlib
+
+# mod = Module()
+# ctx = Context()
+
+# @ctx.capture(rule='<user.text>')
+# def create_element(m):
+#     return '<' + actions.user.formatted_text(m.text, 'PUBLIC_CAMEL_CASE') + '>'
+    
